a8f14f91432c2a78827865fc56f707247400192d
Aurimas Petrevicius <aurimas@notnot.xyz>
2021-03-24 14:50:50 +0200

    sample project use

---
+++ Example/Pods/Target Support Files/Touch/Touch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Touch : NSObject
+@end
+@implementation PodsDummy_Touch
+@end
--- Example/Touch/ViewController.swift
+++ Example/Touch/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import Touch
 
 class ViewController: UIViewController {
 
+    private let widget = Widget(with: "1-TusnfgQuklK4O8n")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        widget.add(to: self)
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        widget.refresh()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
